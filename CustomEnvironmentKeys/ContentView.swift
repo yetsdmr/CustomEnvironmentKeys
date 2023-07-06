cafa1163066570d506c3c84db472c5465f8dc1eb
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-07-06 15:13:00 +0300

    Added environment keys functionality.

--- CustomEnvironmentKeys/ContentView.swift
+++ CustomEnvironmentKeys/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.safeArea) var safeArea
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            Color.red
+                .navigationTitle("EnvironmentKey")
+                .overlay {
+                    Text("Top = \(safeArea.top)\nBottom = \(safeArea.bottom)")
+                    NavigationLink {
+                        DetailView()
+                    } label: {
+                        Text("Detail View")
+                    }
+                    .offset (y: 40)
+                }
         }
-        .padding()
+    }
+}
+
+struct DetailView: View {
+    @Environment(\.safeArea) var safeArea
+    var body: some View {
+        Color.blue
+            .navigationTitle("Detail View")
+            .overlay {
+                Text("Top = \(safeArea.top)\nBottom = \(safeArea.bottom)")
+            }
     }
 }
 
--- CustomEnvironmentKeys/CustomEnvironmentKeysApp.swift
+++ CustomEnvironmentKeys/CustomEnvironmentKeysApp.swift
@@ -10,8 +10,31 @@ import SwiftUI
 @main
 struct CustomEnvironmentKeysApp: App {
     var body: some Scene {
+        // MARK: Now You can Able To Use Custom Environment Keys Anywhere in the SwiftUI View
+        @Environment(\.safeArea) var safeArea
         WindowGroup {
-            ContentView()
+            GeometryReader {
+                let safeArea = $0.safeAreaInsets
+                ContentView()
+                    .padding(safeArea)
+                    .environment(\.safeArea, safeArea)
+                    .ignoresSafeArea(.container, edges: .all)
+            }
+        }
+    }
+}
+
+fileprivate struct SafeAreaValue: EnvironmentKey {
+    static var defaultValue: EdgeInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 0)
+}
+
+extension EnvironmentValues {
+    var safeArea: EdgeInsets {
+        get {
+            self[SafeAreaValue.self]
+        }
+        set {
+            self[SafeAreaValue.self] = newValue
         }
     }
 }
