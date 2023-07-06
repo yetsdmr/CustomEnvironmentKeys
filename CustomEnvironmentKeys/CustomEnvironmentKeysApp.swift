e6bc0205779f12393fc4e6464757c1fcf98ab166
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-07-06 14:55:05 +0300

    Initial Commit

---
+++ CustomEnvironmentKeys/CustomEnvironmentKeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomEnvironmentKeysApp.swift
+//  CustomEnvironmentKeys
+//
+//  Created by Yunus Emre Taşdemir on 6.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomEnvironmentKeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
